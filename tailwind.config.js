40dd632263c957838084e858bd4b33c495a379b0
CodingFlutter <146336747+WebDevVenus@users.noreply.github.com>
2023-10-30 16:34:26 +0200

    update

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        neoPink:'hsl(300, 100%, 50%)',
+        electricBlue:'hsl(220, 90%, 60%)',
+        vibrantPurple:'hsl(270, 80%, 50%)',
+        darkVibrantPurple:'hsl(270, 80%, 40%)',
+        radioactiveGreen: 'hsl(100, 80%, 40%)',
+        glowingOrange: 'hsl(20, 100%, 50%)',
+      },
+      fontFamily: {
+        'sans': ['Bodoni Moda', 'sans-serif'],
+
+      },
+    },
+  },
+  plugins: [],
+}
+
